0a13ad3d6ff99eef0ff7bc2dd63d642e2a4f6da5
LaraibZafar <zafarhassan30@hotmail.com>
2020-10-08 00:23:29 +0500

    added Profile get/myprofile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const router = express.Router();
-
-//@route   GET api/profile
-//@access  Public => you need to be authorized to access this route
-router.get("/", (req, res) => res.send("Profile Route"));
+const auth = require("../../middleware/auth");
+const Profile = require("../../models/Profile");
+const User = require("../../models/User");
+//@route   GET api/profile/myUserID(JWT)
+//@desc    GET current users profile.
+//@access  Private
+router.get("/token", auth, async (req, res) => {
+  try {
+    const profile = await await Profile.findOne({
+      user: req.user.id,
+    }).populate("user", ["name", "avatar"]);
+    if (!profile) {
+      return res.status(400).json({ msg: "Profile not found" });
+    }
+    return res.json(profile);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("GET PROFILE ERROR");
+  }
+});
 
 module.exports = router;
